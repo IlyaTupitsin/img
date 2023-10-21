84d7d9183eb638aa927f45eebd782d1a8a47688e
Илья Тупицин <ilhatupitsin@yandex.ru>
2023-10-21 13:26:21 +0300

    Добавьте файлы проекта.

---
+++ скачивание картинок/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        HttpClient client = new();
+        Console.WriteLine("Введите ссылку для скачивания картинки: ");
+        string nameWebsite = Console.ReadLine();
+        HttpResponseMessage response = await client.GetAsync(nameWebsite);
+        byte[] data = await response.Content.ReadAsByteArrayAsync();
+        await File.WriteAllBytesAsync("C:\\Users\\Пользователь\\Desktop\\b.jpg", data);
+        Process.Start(new ProcessStartInfo { FileName = "C:\\Users\\Пользователь\\Desktop\\b.jpg", UseShellExecute = true });
+    }
+}
